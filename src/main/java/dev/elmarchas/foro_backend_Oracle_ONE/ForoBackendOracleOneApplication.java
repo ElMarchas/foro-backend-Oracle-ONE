aed55109421f1abc5b7d0336d078efd0522071ff
ElMarchas <marchas53@hotmail.com>
2024-07-03 19:50:02 -0600

    first commit

---
+++ src/main/java/dev/elmarchas/foro_backend_Oracle_ONE/ForoBackendOracleOneApplication.java
@@ -0,0 +1,13 @@
+package dev.elmarchas.foro_backend_Oracle_ONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoBackendOracleOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoBackendOracleOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/elmarchas/foro_backend_Oracle_ONE/ForoBackendOracleOneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elmarchas.foro_backend_Oracle_ONE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoBackendOracleOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
